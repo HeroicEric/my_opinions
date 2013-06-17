b69500268e2671df9973b12aae8d9a76e5864e76
Eric Kelly <heroiceric@gmail.com>
2013-06-17 17:04:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyOpinions::Application.config.secret_token = '1c63904291a8a74e510c56a88c92fd6a33ac6bfc2e662997b0ee34a5653b071e2277eccaf84536bbecbcdbc67732025d110c64aadf0670995295dcb4ab58e9f7'
